31545155d79f74fe5b335afee6c446ed36f2be7d
Mehdy Lefkir <mehdylefkir.work@gmail.com>
2023-08-22 13:38:22 +0100

    update simulations documentation

--- src/pioran/utils/psd_decomp.py
+++ src/pioran/utils/psd_decomp.py
@@ -1,6 +1,29 @@
+import jax
 import jax.numpy as jnp
 
-def SHO_power_spectrum(f,A,f0):
+def SHO_power_spectrum(f,A,f0)->jax.Array:
+    """Power spectrum of a simple harmonic oscillator.
+    
+    .. math:: :label: lorentzianpsd 
+    
+       \mathcal{P}(f) = \dfrac{A}{1 + (f-f_0)^4}.
+
+    with the amplitude :math:`A`, the position :math:`f_0\ge 0`.
+    
+
+    Parameters
+    ----------
+    f : :obj:`jax.Array`
+        Frequency array.
+    A : :obj:`float`
+        Amplitude of the model.
+    f0 : :obj:`float`
+        Position of the model.
+
+    Returns
+    -------
+    :obj:`jax.Array`
+    """
     P = A / ( 1 + jnp.power((f/f0),4))
     
     return P
