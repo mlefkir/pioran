a568973a321d6008200ef4c79001ce5fb85f83eb
Mehdy Lefkir <mehdylefkir.work@gmail.com>
2023-09-22 12:34:29 +0100

    remove simu_tests

--- tests/unit/test_simu.py
+++ tests/unit/test_simu.py
@@ -14,22 +14,22 @@ from pioran.psd import OneBendPowerLaw
 
 
 
-class TestPowerSpectrum(unittest.TestCase):
+# class TestPowerSpectrum(unittest.TestCase):
     
-    def setUp(self):
-        self.expo = Exponential([12.1,0.03])
-        self.obpl = OneBendPowerLaw([1,3.2432,4.5,6.7])
-        # self.manualpsd = build_object()
+#     def setUp(self):
+#         self.expo = Exponential([12.1,0.03])
+#         self.obpl = OneBendPowerLaw([1,3.2432,4.5,6.7])
+#         # self.manualpsd = build_object()
         
-    def test_setup_custom_init(self):
+#     def test_setup_custom_init(self):
         
-        # psd = self.manualpsd
-        # self.assertEqual(psd.parameters[1].value,3.1)
-        # self.assertEqual(psd.parameters[2].value,12.3)
-        # self.assertEqual(psd.parameters[1].free,True)
-        # self.assertEqual(psd.parameters[2].free,True)
+#         # psd = self.manualpsd
+#         # self.assertEqual(psd.parameters[1].value,3.1)
+#         # self.assertEqual(psd.parameters[2].value,12.3)
+#         # self.assertEqual(psd.parameters[1].free,True)
+#         # self.assertEqual(psd.parameters[2].free,True)
     
 
     
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
