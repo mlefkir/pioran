4800e65a3f60b3b7c5c6993db8c2fafded32f339
Mehdy Lefkir <mehdylefkir.work@gmail.com>
2024-01-23 22:33:55 +0000

    add two bend power law and correct docs

--- src/pioran/psd.py
+++ src/pioran/psd.py
@@ -53,7 +53,6 @@ class Lorentzian(PowerSpectralDensity):
         r"""Computes the power spectral density.
 
         The expression is given by Equation :math:numref:`lorentzianpsd`.
-        with the variance :math:`A\ge 0`, the position :math:`f_0\ge 0` and the halfwidth :math:`\gamma>0`.
 
         Parameters
         ----------
@@ -186,7 +185,6 @@ class OneBendPowerLaw(PowerSpectralDensity):
         r"""Computes the power spectral density.
 
         The expression is given by Equation :math:numref:`onebendpowerlawpsd`
-        with the variance :math:`A\ge 0` and the scale :math:`\gamma>0`.
 
         Parameters
         ----------
@@ -209,6 +207,7 @@ class OneBendPowerLaw(PowerSpectralDensity):
             1 + jnp.power(f / f_1, index_1 - index_2), -1
         )
         return P * norm
+
 
 class OneBendPowerLawBis(PowerSpectralDensity):
     r"""Other one-bend power-law power spectral density.
@@ -257,7 +256,6 @@ class OneBendPowerLawBis(PowerSpectralDensity):
         r"""Computes the power spectral density.
 
         The expression is given by Equation :math:numref:`onebendpowerlawpsd_bis`
-        with the variance :math:`A\ge 0` and the scale :math:`\gamma>0`.
 
         Parameters
         ----------
@@ -331,7 +329,6 @@ class Matern32PSD(PowerSpectralDensity):
         r"""Computes the power spectral density.
 
         The expression is given by Equation :math:numref:`matern32psd`
-        with the variance :math:`A\ge 0` and the scale :math:`\gamma>0`.
 
         Parameters
         ----------
@@ -350,3 +347,80 @@ class Matern32PSD(PowerSpectralDensity):
             / self.parameters["scale"].value ** 3
             / (3 / self.parameters["scale"].value ** 2 + 4 * jnp.pi**2 * f**2) ** 2
         )
+
+
+class TwoBendPowerLaw(PowerSpectralDensity):
+    r"""Two-bend power-law power spectral density.
+
+    .. math:: :label: twobendpowerlawpsd
+
+        \mathcal{P}(f) = A\times (f/f_1)^{-\alpha_1} \frac{1}{1+(f/f_1)^{(\alpha_2-\alpha_1)}} \frac{1}{1+(f/f_2)^{(\alpha_3-\alpha_2)}}
+
+    with the amplitude :math:`A\ge 0`, the bend frequencies :math:`f_1\ge 0`,:math:`f_2\ge f_1` and the indices :math:`\alpha_1,\alpha_2,\alpha_3`.
+
+    Parameters
+    ----------
+    param_values : :obj:`list` of :obj:`float`
+        Values of the parameters of the power spectral density function.
+        In order: [norm, index_1, freq_1, index_2]
+    free_parameters : :obj:`list` of :obj:`bool`, optional
+        List of bool to indicate if the parameters are free or not. Default is `[False, True, True,True]`.
+
+    """
+
+    expression = "twobend-powerlaw"
+    """Expression of the power spectral density function."""
+    parameters: ParametersModel
+    """Parameters of the power spectral density function."""
+    analytical = False
+    """If True, the power spectral density function is analytical, otherwise it is not."""
+
+    def __init__(
+        self, parameters_values, free_parameters=[False, True, True, True, True, True]
+    ):
+        assert (
+            len(parameters_values) == 6
+        ), f"The number of parameters for onebend-powerlaw must be 6, not {len(parameters_values)}"
+        assert (
+            len(free_parameters) == 6
+        ), f"The number of free parameters for onebend-powerlaw must be 6, not {len(free_parameters)}"
+        # initialise the parameters and check
+        names = ["norm", "index_1", "freq_1", "index_2", "freq_2", "index_3"]
+
+        PowerSpectralDensity.__init__(
+            self,
+            param_values=parameters_values,
+            param_names=names,
+            free_parameters=free_parameters,
+        )
+
+    def calculate(self, f):
+        r"""Computes the power spectral density.
+
+        The expression is given by Equation :math:numref:`twobendpowerlawpsd`
+
+        Parameters
+        ----------
+        f : :obj:`jax.Array`
+            Array of frequencies.
+
+        Returns
+        -------
+        :obj:`jax.Array`
+            Power spectral density function evaluated on the array of frequencies.
+        """
+
+        index_1, f_1, index_2, f_2, index_3, norm = (
+            self.parameters["index_1"].value,
+            self.parameters["freq_1"].value,
+            self.parameters["index_2"].value,
+            self.parameters["freq_2"].value,
+            self.parameters["index_3"].value,
+            self.parameters["norm"].value,
+        )
+        P = (
+            jnp.power(f / f_1, -index_1)
+            * jnp.power(1 + jnp.power(f / f_1, index_2 - index_1), -1)
+            * jnp.power(1 + jnp.power(f / f_2, index_3 - index_2), -1)
+        )
+        return P * norm
